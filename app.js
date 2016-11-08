0b6640f0535bb4fe9ec9bffcdda3a6eee6ab0d0f
Guillermo Chavez <gchavez@infonet.com.gt>
2016-11-08 11:43:52 -0600

    CORS... maybe....?

--- app.js
+++ app.js
@@ -55,6 +55,10 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 module.exports = app;
